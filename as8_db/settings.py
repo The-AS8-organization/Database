c771003fef571d3472a69cd7cc7e93424b0c6ac9
Ahammad Shawki <ahammadshawki8@gmail.com>
2021-12-09 10:32:11 +0600

    minor bug fixs

--- as8_db/settings.py
+++ as8_db/settings.py
@@ -18,9 +18,7 @@ import django_heroku
 BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 CORE_TEMPLATE_DIR = os.path.join(BASE_DIR, "core/templates/core/")
-RESOURCE_TEMPLATE_DIR = os.path.join(BASE_DIR, "resource/templates/resource/")
 CORE_STATIC_DIR = os.path.join(BASE_DIR, "core/static/core/")
-RESOURCE_STATIC_DIR = os.path.join(BASE_DIR, "resource/static/resource/")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -62,7 +60,7 @@ ROOT_URLCONF = 'as8_db.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_TEMPLATE_DIR, CORE_TEMPLATE_DIR, RESOURCE_TEMPLATE_DIR],
+        'DIRS': [BASE_TEMPLATE_DIR, CORE_TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [CORE_STATIC_DIR, RESOURCE_STATIC_DIR]
+STATICFILES_DIRS = [CORE_STATIC_DIR]
 
 # Login
 LOGIN_URL = '/core/user_login'
